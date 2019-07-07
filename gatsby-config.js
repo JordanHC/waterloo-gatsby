45a2629f28022c849e928ce663cff40544bffb20
Jordan Hale-Clowes <>
2019-07-07 12:58:03 +0100

    using description instead of excerpt

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
+                  description: edge.node.frontmatter.description,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
@@ -109,6 +109,7 @@ module.exports = {
                       fields { slug }
                       frontmatter {
                         title
+                        description
                         date
                       }
                     }
